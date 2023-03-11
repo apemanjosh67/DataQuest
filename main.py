0f47c97c7ed57095d7397f453ffbb9185c4a5dcf
Josh <joshmuszka67@gmail.com>
2023-03-11 14:01:42 -0500

    put into methods

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import time
 from sklearn.linear_model import LinearRegression
 import numpy as numpy
 import matplotlib.pylab as plt
-
-data = open("trainingdata.csv", "r")
-lines = data.readlines()
-lines.pop(0)
-
-DATA = []
+import math
 
 booking_id = 0
 lead_time = 1
@@ -30,28 +25,45 @@ avg_room_price = 16
 special_requests = 17
 booking_status = 18
 
-for i in range(18):
-    DATA.append([])
+def get_data(filename):
 
-for line in lines:
-    line_data = line.split(",")
-    for i in range(len(DATA)):
-        DATA[i].append(line_data[i])
+    data = open(filename, "r")
+    lines = data.readlines()
+    lines.pop(0)
+
+    DATA = []
 
 
+    for i in range(18):
+        DATA.append([])
 
-x = []
-for i in range(len(DATA[arrival_date])-1):
-    arr = [DATA[arrival_date][i], DATA[arrival_date][i+1]]
-    for j in range(len(arr)):
-        arr[j] = int(arr[j])
-    x.append(arr)
-X = numpy.array(x)
-Y = X
+    for line in lines:
+        line_data = line.split(",")
+        for i in range(len(DATA)):
+            DATA[i].append(line_data[i])
+
+    return DATA
+
+def get_narray(array):
+    x = []
+    for i in range(len(array)-1):
+        arr = [array[i], array[i+1]]
+        for j in range(len(arr)):
+            arr[j] = int(math.floor(float(arr[j])))
+        x.append(arr)
+
+    return numpy.array(x)
+
+
+DATA = get_data("trainingdata.csv")
+
+X = get_narray(DATA[num_adults])
+Y = get_narray(DATA[avg_room_price])
 
 
 mod = LinearRegression().fit(X,Y)
-pred = mod.predict(X)
+#pred = mod.predict(X)
 
-plt.scatter(pred,Y)
+#plt.scatter(pred,Y)
+plt.scatter(X,Y)
 plt.show()
