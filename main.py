b52e170e5be779e01fabb34737f6c86eb8ea2266
Josh <joshmuszka67@gmail.com>
2023-03-11 16:18:46 -0500

    stuff

--- main.py
+++ main.py
@@ -15,23 +15,54 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
+pd.options.display.max_columns = None
+
+
+# Function for replacing string data into numeric values
+def get_numeric_value(datf, items):
+    for i in range(len(items)):
+        datf = datf.replace({items[i]: i})
+    return datf
+
 #Get the Dataframe
 df = pd.read_csv('trainingdata.csv')
-print (df.head)
+#print (df.head)
 
 #Change the string values to numerical values
 df['BookingStatus'] = df['BookingStatus'].replace({'Not_Canceled':0, 'Canceled': 1})
-print (df.head)
+#print (df.head)
 
 df['RoomType'] = df['BookingStatus'].replace({'Room_Type 1': 1, 'Room_Type 2': 2,'Room_Type 3': 3,'Room_Type 4': 4,'Room_Type 5': 5,'Room_Type 6': 6})
-print (df.head)
+#print (df.head)
 
 df['MealPlan'] = df['MealPlan'].replace({'Not Selected': 0, 'Meal Plan 1': 1, 'Meal Plan 2' : 2})
-print (df.head)
+#print (df.head)
 
 df['MarketSegment'] = df['MarketSegment'].replace({'Offline' : 0,'Online' : 1,'Corporate' : 2,'Complementary' : 3})
 
+#Remove parking, arrival year, repeated guest (PREDICT BOOKING STATUS)
+df = df.drop(columns=['Parking', 'ArrivalYear', 'RepeatedGuest'])
 
+#Add number of children and adults
+#to make number of people column
+i = 0
+for item in df['NumAdults']:
+    df['NumAdults'][i] = df['NumAdults'][i] + df['NumChildren'][i]
+    i+=1
+df = df.rename(columns={"NumAdults":"NumPeople"})
+
+#Change NumChildren to HasChildren
+i = 0
+for item in df['NumChildren']:
+    num = df['NumChildren'][i]
+
+    if num > 0:
+        df['NumChildren'][i] = 1
+    else:
+        df['NumChildren'][i] = 0
+    
+    i+=1
+df = df.rename(columns={'NumChildren':'HasChildren'})
 
 #grouped_df = df.groupby('RoomType')
 
