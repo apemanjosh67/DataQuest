533f5cb3081155e19b3a8b1929b0c372b2783294
ExplorerDonutz <mwquick04@gmail.com>
2023-03-11 16:13:44 -0500

    Created method to replace string data with numeric values

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set_theme()
 sns.set_palette(sns.color_palette(['#851836', '#edbd17']))
 sns.set_style("darkgrid")
@@ -15,28 +16,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
-#Get the Dataframe
+
+# Function for replacing string data into numeric values
+def get_numeric_value(datf, items):
+    for i in range(len(items)):
+        datf = datf.replace({items[i]: i})
+    return datf
+
+
+# Get the Dataframe
 df = pd.read_csv('trainingdata.csv')
-print (df.head)
 
-#Change the string values to numerical values
-df['BookingStatus'] = df['BookingStatus'].replace({'Not_Canceled':0, 'Canceled': 1})
-print (df.head)
+# Replace all string data with numeric values
+df['MealPlan'] = get_numeric_value(df['MealPlan'], ['Not Selected', 'Meal Plan 1', 'Meal Plan 2'])
 
-df['RoomType'] = df['BookingStatus'].replace({'Room_Type 1': 1, 'Room_Type 2': 2,'Room_Type 3': 3,'Room_Type 4': 4,'Room_Type 5': 5,'Room_Type 6': 6})
-print (df.head)
+df['RoomType'] = get_numeric_value(df['RoomType'], ['Room_Type 1', 'Room_Type 2', 'Room_Type 3', 'Room_Type 4', 'Room_Type 5', 'Room_Type 6'])
 
-df['MealPlan'] = df['MealPlan'].replace({'Not Selected': 0, 'Meal Plan 1': 1, 'Meal Plan 2' : 2})
-print (df.head)
+df['MarketSegment'] = get_numeric_value(df['MarketSegment'], ['Offline', 'Online', 'Corporate', 'Complementary'])
 
-df['MarketSegment'] = df['MarketSegment'].replace({'Offline' : 0,'Online' : 1,'Corporate' : 2,'Complementary' : 3})
+df['BookingStatus'] = get_numeric_value(df['BookingStatus'], ['Canceled', 'Not_Canceled'])
 
+print(df.head)
 
+# grouped_df = df.groupby('RoomType')
 
-#grouped_df = df.groupby('RoomType')
+# df[['AvgPriceMean', 'AdultCount','ChildCount','AvgNumWeekNights', 'AvgNumWeekendNights']] = grouped_df[['AvgRoomPrice', 'NumAdults','NumChildren', 'NumWeekNights', 'NumWeekendNights']].transform('mean')
+# df[['Canceled']] = grouped_df[['BookingStatus']].transform('sum')
+# pd.set_option('display.max_columns', None)
 
-#df[['AvgPriceMean', 'AdultCount','ChildCount','AvgNumWeekNights', 'AvgNumWeekendNights']] = grouped_df[['AvgRoomPrice', 'NumAdults','NumChildren', 'NumWeekNights', 'NumWeekendNights']].transform('mean')
-#df[['Canceled']] = grouped_df[['BookingStatus']].transform('sum')
-#pd.set_option('display.max_columns', None)
-
-#print (df[['RoomType', 'AvgPriceMean','AvgNumWeekNights', 'AvgNumWeekendNights', 'AdultCount', 'ChildCount', 'Canceled']].head(50))
+# print (df[['RoomType', 'AvgPriceMean','AvgNumWeekNights', 'AvgNumWeekendNights', 'AdultCount', 'ChildCount', 'Canceled']].head(50))
